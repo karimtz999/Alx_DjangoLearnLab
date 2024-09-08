80a389f141bd8f745a69099f6bc1a21416db10dc
ELFA <140125432+karimtz999@users.noreply.github.com>
2024-09-08 18:20:57 +0100

    Update urls.py

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('books/', ListView.as_view(), name='book-list'),  # List all books
     path('books/<int:pk>/', DetailView.as_view(), name='book-detail'),  # Retrieve a single book
     path('books/create/', CreateView.as_view(), name='book-create'),  # Create a new book
-    path('books/<int:pk>/update/', UpdateView.as_view(), name='book-update'),  # Update an existing book
-    path('books/<int:pk>/delete/', DeleteView.as_view(), name='book-delete'),  # Delete a book
+    path('books/update/', UpdateView.as_view(), name='book-update'),  # Update an existing book
+    path('books/delete/', DeleteView.as_view(), name='book-delete'),  # Delete a book
 ]
