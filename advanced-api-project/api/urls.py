a25bc5bf5282003e2a901c02b12ff0d039a7d474
ELFA <140125432+karimtz999@users.noreply.github.com>
2024-09-08 18:12:55 +0100

    Update urls.py

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import BookListView, BookDetailView
+from .views import CreateView, UpdateView, DeleteView
 
 urlpatterns = [
-    path('books/', BookListView.as_view(), name='book-list'),
-    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
+    path('books/create/', CreateView.as_view(), name='book-create'),
+    path('books/<int:pk>/update/', UpdateView.as_view(), name='book-update'),
+    path('books/<int:pk>/delete/', DeleteView.as_view(), name='book-delete'),
 ]
