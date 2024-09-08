a14373eb395d57879adfed6426bbb1863f8c63db
ELFA <140125432+karimtz999@users.noreply.github.com>
2024-09-08 18:12:34 +0100

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,20 +1,6 @@
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
-from rest_framework.filters import SearchFilter
-
-class BookListView(generics.ListCreateAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-    permission_classes = [IsAuthenticated]
-    filter_backends = [SearchFilter]
-    search_fields = ['title']
-
-class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-    permission_classes = [IsAdminUser]
 
 # CreateView for creating a new Book instance
 class CreateView(generics.CreateAPIView):
