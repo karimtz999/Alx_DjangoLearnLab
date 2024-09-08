c2635d54f6fa1a07ae97de5ddd6572d05c76d252
ELFA <140125432+karimtz999@users.noreply.github.com>
2024-09-08 18:09:18 +0100

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -15,3 +15,18 @@ class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAdminUser]
+
+# CreateView for creating a new Book instance
+class CreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# UpdateView for updating an existing Book instance
+class UpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# DeleteView for deleting an existing Book instance
+class DeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
