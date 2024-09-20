a1ea89cef75f52af4cfaabac2d330564d2c86b4c
ELFA <140125432+karimtz999@users.noreply.github.com>
2024-09-20 13:55:23 +0100

    Update serializers.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -9,23 +9,23 @@ from django.contrib.auth.models import User
 CustomUser = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)  # Password should not be readable
+    password = serializers.CharField(write_only=True)  # Password should not be returned in responses
     
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'password', 'bio', 'profile_picture']
     
     def create(self, validated_data):
-        # Use the `create_user` method to properly handle password hashing
+        # Create a new user using `create_user` method
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email', ''),
-            password=validated_data['password'],
+            password=validated_data['password'],  # Password is hashed internally by create_user()
             bio=validated_data.get('bio', ''),
             profile_picture=validated_data.get('profile_picture', None)
         )
         
-        # Create a token for the user
+        # Create a token for the new user
         Token.objects.create(user=user)
         
         return user
