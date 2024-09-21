51c828221c1964409c12a6ccec53e4685bee1e75
ELFA <140125432+karimtz999@users.noreply.github.com>
2024-09-21 23:35:21 +0100

    Update urls.py

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,10 +1,11 @@
 # posts/urls.py
 
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, CommentViewSet
+from .views import PostViewSet, CommentViewSet, FeedView
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)  # Only one registration
 router.register(r'comments', CommentViewSet)  # Ensure this is not duplicated
+router.register(r'feed', FeedView)
 
 urlpatterns = router.urls
